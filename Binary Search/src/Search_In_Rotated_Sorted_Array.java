3aa6cc6d7ef7d89c14ccfdea24fcc85573f7ab7a
VishnuKant0925 <vishnukant844121@gmail.com>
2025-12-01 22:53:43 +0530

    You are given an array of numbers that was originally sorted but might have been rotated from some point. You need to find a given target number inside this rotated array. Return the index of the target if it exists, otherwise return -1, and your solution must be fast (log n time).

---
+++ Binary Search/src/Search_In_Rotated_Sorted_Array.java
@@ -0,0 +1,29 @@
+public class Search_In_Rotated_Sorted_Array {
+    public static int search(int[] arr, int target) {
+        int  n  = arr.length;
+        int low = 0;
+        int high= n-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]==target){
+                return mid;
+            }else if(arr[mid]<=arr[high]){   // right search space
+                if(arr[mid]<target && target<=arr[high]){
+                    low=mid+1;
+                }else high = mid-1;
+            }else{
+                if(arr[low]<=target && target<arr[mid]){
+                    high=mid-1;
+                }else low=mid+1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={4,5,6,7,0,1,2};
+        int target=4;
+        int res= search(arr,target);
+        System.out.println("The target found at :"+res);
+    }
+}
