e78ee26f2cc3f5b45adc2010537c439873870853
VishnuKant0925 <vishnukant844121@gmail.com>
2025-12-07 11:08:20 +0530

    Implemented the recursive function to calculate the sum of n numbers (Using Return type)

--- Recursion/src/Print_1_To_N_RECURSIVECALL.java
+++ Recursion/src/Print_1_To_N_RECURSIVECALL.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Print_1_To_N_RECURSIVECALL {
+import java.util.Scanner;
+
+public class Print_1_To_N_RECURSIVECALL {
+    public static void Print(int n ){
+        if(n==0) return;   //basecase
+        Print(n-1);   // recursive call
+        System.out.print(n+" "); //work
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n: ");
+        int n = sc.nextInt();
+        Print(n);
+    }
 }
