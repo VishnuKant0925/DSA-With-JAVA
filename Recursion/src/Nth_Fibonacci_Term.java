a824fa7c2be4572e75fa886677a527a8f2e9d2f9
VishnuKant0925 <vishnukant844121@gmail.com>
2025-12-07 18:31:07 +0530

    Fibonacci through recursion

--- Recursion/src/Nth_Fibonacci_Term.java
+++ Recursion/src/Nth_Fibonacci_Term.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Nth_Fibonacci_Term {
+import java.util.Scanner;
+
+public class Nth_Fibonacci_Term {
+    public static int Fibo(int n){
+        if(n<=1) return n;
+        return Fibo(n-1)+Fibo(n-2);
+    }
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter  n :  ");
+        int n = sc.nextInt();
+        int res=Fibo(n);
+        System.out.println(n+"th Fibonacci term is "+res);
+    }
 }
