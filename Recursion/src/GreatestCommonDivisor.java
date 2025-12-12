4ab8db1a11883ff8b06916da59e46e5844905301
VishnuKant0925 <vishnukant844121@gmail.com>
2025-12-12 18:34:38 +0530

    Added the gcd looping function into the code having time complexity O(min(a,b))

--- Recursion/src/GreatestCommonDivisor.java
+++ Recursion/src/GreatestCommonDivisor.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 
 public class GreatestCommonDivisor {
-    public static int gcd(int a, int b){
+    public static int gcd(int a, int b){  // gcd using recursion
         if(b%a==0) return a;
         return gcd(b%a,a);
+    }
+    public static int hcf(int a, int b){ //gcd using loops
+        for(int i=Math.min(a,b);i>=1;i--){
+            if(a%i==0 && b%i==0) return i;
+        }
+        return 1;
     }
     public static void main (String[] args)
     {
@@ -14,5 +20,6 @@ public class GreatestCommonDivisor {
         int b=sc.nextInt();
         int res=gcd(a,b);
         System.out.println(res);
+        System.out.println("Using loop :"+hcf(a,b));
     }
 }
