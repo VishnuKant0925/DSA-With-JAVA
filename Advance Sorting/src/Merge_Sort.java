2dc225c64e1f374b68e5be9039a79205dfcd7009
VishnuKant0925 <vishnukant844121@gmail.com>
2025-12-14 09:24:58 +0530

    MERGE_SORT CODE

---
+++ Advance Sorting/src/Merge_Sort.java
@@ -0,0 +1,44 @@
+public class Merge_Sort {
+    public static void print(int [] arr){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void merge(int [] a , int[] b, int[] c){
+        int i=0 ,j=0,k=0;
+        while(i<a.length && j<b.length){
+            if(a[i]<b[j])  c[k++]=a[i++];
+            else c[k++]=b[j++];
+        }
+        while(i<a.length) c[k++]=a[i++];
+        while(j<b.length) c[k++]=b[j++];
+    }
+    public static void mergesort(int[] arr){
+        int n=arr.length;
+        if(n==1) return;  // base case
+        //create two array of n/2 size each
+        int[] a= new int[n/2];
+        int[] b= new int[n-n/2];
+        //copy elements
+        for(int i=0;i<n/2;i++){
+            a[i]=arr[i];
+        }
+        for(int i=0;i<n-n/2;i++){
+            b[i]=arr[i+n/2];
+        }
+        //magic(recursion)
+        mergesort(a);
+        mergesort(b);
+        //merge these 'a' and 'b'
+        merge(a,b,arr);
+
+    }
+    public static void main(String[] args) {
+        int [] arr={80,10,105,1,30,60,40,50,20};
+        print(arr);
+        mergesort(arr);
+        print(arr);
+    }
+}
