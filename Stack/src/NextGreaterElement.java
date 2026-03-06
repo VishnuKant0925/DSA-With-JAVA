75ab31bd98a46443add7f8f1e3d119984a6ba860
VishnuKant0925 <vishnukant844121@gmail.com>
2026-03-06 18:45:00 +0530

    Next greater element using stack

---
+++ Stack/src/NextGreaterElement.java
@@ -0,0 +1,32 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class NextGreaterElement
+{
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            ans[i]=-1;
+        }
+        Stack<Integer> st = new Stack<>();
+        st.push(arr[n-1]);
+        for(int i=n-2;i>=0;i--){
+            while(!st.isEmpty() && st.peek()<=arr[i]) st.pop();
+            ans[i]=st.peek();
+            st.push(arr[i]);
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+}
+
