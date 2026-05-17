a569ec73b9a6e4338269d00380d6abee61594c8e
VishnuKant0925 <vishnukant844121@gmail.com>
2026-05-17 21:46:45 +0530

    implemented recursive function to calculate sum , product and product of non-zero node of tree

--- Tree/src/SumOfTreeNode.java
+++ Tree/src/SumOfTreeNode.java
@@ -14,16 +14,34 @@ public class SumOfTreeNode {
         display(root.left);
         display(root.right);
     }
-    public static  int sum (Node root){
+    private static  int sum (Node root){
         if(root==null){
             return 0;
         }
         return root.val+sum(root.left) + sum(root.right);
     }
+    private static  int product (Node root){
+        if(root == null ){
+            return 1;
+        }
+        return root.val * product(root.left) * product(root.right);
+    }
+
+    private static  int NonZeroProduct (Node root){
+        if(root == null ) {
+            return 1;
+        }
+        if(root.val==0){
+            return NonZeroProduct(root.left) * NonZeroProduct(root.right);
+        }else {
+            return root.val * NonZeroProduct(root.left) * NonZeroProduct(root.right);
+        }
+    }
+
     public static void main(String[] args) {
         Node a = new Node(1); // a is root
         Node b = new Node(10);
-        Node c = new Node(2);
+        Node c = new Node(0);
         Node d = new Node(20);
         Node e = new Node(3);
         Node f = new Node(30);
@@ -37,6 +55,8 @@ public class SumOfTreeNode {
         display(a);
         System.out.println();
         System.out.println(sum(a));
+        System.out.println(product(a));
+        System.out.println(NonZeroProduct(a));
     }
 }
 
