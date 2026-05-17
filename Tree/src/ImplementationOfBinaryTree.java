0ef7684bb7dd91206e30fcfa1b69f2951fe94f67
VishnuKant0925 <vishnukant844121@gmail.com>
2026-05-17 21:24:56 +0530

    IMPLEMENTATION OF BINARY TREE

---
+++ Tree/src/ImplementationOfBinaryTree.java
@@ -0,0 +1,34 @@
+public class ImplementationOfBinaryTree {
+    static  class Node{
+        int val;
+        Node left;
+        Node right;
+        Node(int val){
+            this.val=val;
+        }
+    }
+
+    private static void display(Node root){
+        if(root==null) return ; // base-case
+        System.out.print(root.val+" ");
+        display(root.left);
+        display(root.right);
+    }
+    public static void main(String[] args) {
+        Node a = new Node(1); // a is root
+        Node b = new Node(10);
+        Node c = new Node(2);
+        Node d = new Node(20);
+        Node e = new Node(3);
+        Node f = new Node(30);
+        Node g = new Node(40);
+
+        // connect the children
+        a.left = b ; a.right = c;
+        b.left = d ; b.right = e;
+        c.right = f; c.left = g ;
+
+        display(a);
+
+    }
+}
