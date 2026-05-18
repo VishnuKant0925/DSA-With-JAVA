f76c2e4d71014306888c386c0b78b0d63b0b0570
VishnuKant0925 <vishnukant844121@gmail.com>
2026-05-18 10:46:51 +0530

    implemented functions to find level,size,max,min value of tree

--- Tree/src/SumOfTreeNode.java
+++ Tree/src/SumOfTreeNode.java
@@ -38,10 +38,36 @@ public class SumOfTreeNode {
         }
     }
 
+    private static int maxTreeNode (Node root){
+        if(root==null) return Integer.MIN_VALUE;
+        int a = root.val;
+        int b = maxTreeNode(root.left);
+        int c = maxTreeNode(root.right);
+        return Math.max(a,Math.max(b,c));
+    }
+
+    private static int minTreeNode (Node root){
+        if(root==null) return Integer.MAX_VALUE;
+        int a = root.val;
+        int b = minTreeNode(root.left);
+        int c = minTreeNode(root.right);
+        return Math.min(a,Math.min(b,c));
+    }
+
+    private static int sizeOfTree(Node root){
+        if(root==null) return 0;
+        return 1+sizeOfTree(root.left)+sizeOfTree(root.right);
+    }
+
+    private static int levelOfTree(Node root){
+        if(root==null) return 0;
+        return 1 + Math.max( levelOfTree(root.left) , levelOfTree(root.right) );
+    }
+
     public static void main(String[] args) {
         Node a = new Node(1); // a is root
         Node b = new Node(10);
-        Node c = new Node(0);
+        Node c = new Node(9);
         Node d = new Node(20);
         Node e = new Node(3);
         Node f = new Node(30);
@@ -57,6 +83,10 @@ public class SumOfTreeNode {
         System.out.println(sum(a));
         System.out.println(product(a));
         System.out.println(NonZeroProduct(a));
+        System.out.println(maxTreeNode(a));
+        System.out.println(minTreeNode(a));
+        System.out.println(sizeOfTree(a));
+        System.out.println(levelOfTree(a));
     }
 }
 
