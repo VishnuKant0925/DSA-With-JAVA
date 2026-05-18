0235e308b67d422fc7669396d9f103cba16f0888
VishnuKant0925 <vishnukant844121@gmail.com>
2026-05-18 16:46:56 +0530

    tree traversals

---
+++ Tree/src/Traversal.java
@@ -0,0 +1,88 @@
+
+public class Traversal {
+    static class Node{
+        int val;
+        Node left;
+        Node right;
+        Node(int val){
+            this.val=val;
+        }
+    }
+    private static void display(Node root){
+        if(root==null) return ; // base-case
+        System.out.print(root.val+" ");
+        display(root.left);
+        display(root.right);
+    }
+    private static void preorder(Node root){
+        if(root==null) return ; // base-case
+        System.out.print(root.val+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    private static void inorder(Node root){
+        if(root==null) return ; // base-case
+
+        inorder(root.left);
+        System.out.print(root.val+" ");
+        inorder(root.right);
+    }
+
+    private static void postorder(Node root){
+        if(root==null) return ; // base-case
+
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.val+" ");
+    }
+
+    private static void ReversePreorder(Node root){
+        if(root==null) return ; // base-case
+        System.out.print(root.val+" ");
+        ReversePreorder(root.right);
+        ReversePreorder(root.left);
+    }
+    private static void ReverseInorder(Node root){
+        if(root==null) return ; // base-case
+
+        ReverseInorder(root.right);
+        System.out.print(root.val+" ");
+        ReverseInorder(root.left);
+    }
+
+    private static void ReversePostorder(Node root){
+        if(root==null) return ; // base-case
+
+        ReversePostorder(root.right);
+        ReversePostorder(root.left);
+        System.out.print(root.val+" ");
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(1); // a is root
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        Node g = new Node(7);
+        Node h = new Node(8);
+        Node i = new Node(9);
+
+        // connect the children
+        a.left = b ; a.right = c;
+        b.left = d ; b.right = e;
+        c.left = f; c.right = g ;
+        e.left = h ; f.right = i;
+        display(a);
+        System.out.println();
+        System.out.print("Preorder: ");
+        preorder(a);
+        System.out.println();
+        System.out.print("Inorder: ");
+        inorder(a);
+        System.out.println();
+        System.out.print("Postorder: ");
+        postorder(a);
+    }
+}
